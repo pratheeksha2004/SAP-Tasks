0098007796d6762d8ac107e630920ce19145b4a1
Pratheeksha K N <pratheeksharaju2004@gmail.com>
2025-04-29 14:28:55 +0000

    added features

--- html5app/webapp/controller/View1.controller.js
+++ html5app/webapp/controller/View1.controller.js
@@ -1,34 +1,61 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("com.pratheeksha.ui.html5app.controller.View1", {
-        
+
         onInit: function () {
-            // Create empty model and set it to view
             var oModel = new JSONModel({
                 name: "",
-                age: ""
+                age: "",
+                details: []
             });
             this.getView().setModel(oModel, "formModel");
         },
 
-        onSavePress: function (oEvent) {
+        onSavePress: function () {
             var oView = this.getView();
-            
-            // Get values from inputs
-            var sName = oView.byId("nameInput").getValue();
-            var sAge = oView.byId("ageInput").getValue();
-
-            // Update JSON model
             var oModel = oView.getModel("formModel");
-            oModel.setProperty("/name", sName);
-            oModel.setProperty("/age", sAge);
 
-            // Show a Message Toast
-            sap.m.MessageToast.show("Data Saved: " + sName + ", Age: " + sAge);
+            var sName = oView.byId("idInputName").getValue().trim();
+            var sAge = oView.byId("idInputAge").getValue().trim();
+
+            if (!sName || !sAge) {
+                MessageToast.show("Please enter both Name and Age.");
+                return;
+            }
+
+            var aDetails = oModel.getProperty("/details") || [];
+
+            aDetails.push({
+                name: sName,
+                age: sAge
+            });
+
+            oModel.setProperty("/details", aDetails);
+
+            // Clear input fields
+            oView.byId("idInputName").setValue("");
+            oView.byId("idInputAge").setValue("");
+
+            MessageToast.show("Saved: " + sName + ", Age: " + sAge);
+        },
+
+        onDeletePress: function (oEvent) {
+            var oModel = this.getView().getModel("formModel");
+            var aDetails = oModel.getProperty("/details");
+
+            var oItem = oEvent.getSource().getParent(); // ColumnListItem
+            var oCtx = oItem.getBindingContext("formModel");
+            var iIndex = oCtx.getPath().split("/").pop(); // Get index
+
+            aDetails.splice(iIndex, 1); // Remove item
+            oModel.setProperty("/details", aDetails);
+
+            MessageToast.show("Item deleted.");
         }
     });
 });
